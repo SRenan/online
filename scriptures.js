2ef5862c48772b00c8a89f68c14893d621b8a0cd
dubiousdata <99302753+dubiousdata@users.noreply.github.com>
2024-04-05 05:56:28 -0400

    Update scriptures.js

--- scriptures.js
+++ scriptures.js
@@ -1,34 +1,35 @@
-function myFunction() {
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent default form submission
 
-var nbGames = document.getElementById("nbGames").value;
-console.log(nbGames);
-var steamID = document.getElementById("steamID").value;
-console.log(steamID);
-var apiUrl = "https://aoe2.net/api/player/matches?game=aoe2de&steam_id=" + steamID + "&count=" + nbGames;
-console.log(apiUrl);
+        var nbGames = document.getElementById("nbGames").value;
+        console.log(nbGames);
+        var steamID = document.getElementById("steamID").value;
+        console.log(steamID);
+        var apiUrl = "https://aoe2.net/api/player/matches?game=aoe2de&steam_id=" + steamID + "&count=" + nbGames;
+        console.log(apiUrl);
 
-event.preventDefault()
-fetch(apiUrl).then((data)=>{
-	console.log(data);
-	return data.json();
-}).then((completedata)=>{
-	console.log(completedata[0]);
-	let data1="";
-	completedata.map((values)=>{
-		data1+=` <div class="cards">
-		<h3 class="matchID">${values.match_id}</h3>
-			<ul>
-				<li>Average rating : ${values.average_rating}</li>
-				<li>Number of players : ${values.num_players}</li>
-				<li>Map type : ${values.map_type}</li>
-				<li>Victory : ${values.victory}</li>
-			</ul> 
-	</div>`;
-	});
-	document.getElementById("cards").innerHTML=data1;
-	
-}).catch((err)=>{
-	console.log(err);
-})
-}
-	
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(completedata => {
+                console.log(completedata[0]);
+                let data1 = "";
+                completedata.forEach(values => {
+                    data1 += `
+                        <div class="cards">
+                            <h3 class="matchID">${values.match_id}</h3>
+                            <ul>
+                                <li>Average rating : ${values.average_rating}</li>
+                                <li>Number of players : ${values.num_players}</li>
+                                <li>Map type : ${values.map_type}</li>
+                                <li>Victory : ${values.victory}</li>
+                            </ul> 
+                        </div>`;
+                });
+                document.getElementById("cards").innerHTML = data1;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    });
+});
