c5db0dfcda2178da2be0a7405ea2c4c2257b1911
dubiousdata <99302753+dubiousdata@users.noreply.github.com>
2024-04-11 13:13:38 -0400

    Update khaserscript.js

--- khaser/khaserscript.js
+++ khaser/khaserscript.js
@@ -20,9 +20,25 @@ function preload() {
 // Create game objects and set up initial game state
 function create() {
     // Add your create code here
+    
+    // Create a red rectangle at the bottom
+    player = this.add.rectangle(150, 575, 50, 25, 0xff0000);
+
+    // Enable keyboard input
+    this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 // Update game logic in each frame
 function update() {
     // Add your update code here
+    
+    // Move the player left
+    if (this.cursors.left.isDown) {
+        player.x -= 5;
+    }
+    
+    // Move the player right
+    if (this.cursors.right.isDown) {
+        player.x += 5;
+    }
 }
