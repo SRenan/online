129a37dde9cfdcea2c3d566a28f9c1f5b34f2bdb
dubiousdata <99302753+dubiousdata@users.noreply.github.com>
2024-04-30 17:17:57 -0400

    Update Scene1.js

--- khaser/scenes/Scene1.js
+++ khaser/scenes/Scene1.js
@@ -1,1 +1,16 @@
+var Scene1 = {
+    create: function() {
+        // Get the elapsed time from the data passed to the scene
+        var elapsedTime = this.scene.settings.data.elapsedTime;
+
+        // Display "Game Over" text
+        this.add.text(200, 200, 'Game Over', { fontSize: '32px', fill: '#ffffff' });
+
+        // Display the message with elapsed time
+        this.add.text(150, 250, 'You\'ve reached 50 in ' + (elapsedTime / 1000).toFixed(2) + ' seconds', { fontSize: '24px', fill: '#ffffff' });
+    }
+};
+
+// Export the Scene1 object
+module.exports = Scene1;
 
