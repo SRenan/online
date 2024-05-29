20213b944481e5e6cad6e1c09d8cedc3bdc48890
dubiousdata <99302753+dubiousdata@users.noreply.github.com>
2024-05-29 16:11:15 -0400

    Update Scene2_mainMenu.js

--- TurquiePeutOnCompter/Scene2_mainMenu.js
+++ TurquiePeutOnCompter/Scene2_mainMenu.js
@@ -13,10 +13,10 @@ class Scene2 extends Phaser.Scene{
 		
 		// Add a button
 		
-		let buttonTakeoff = this.add.image(config.width / 2, config.height / 3, 'buttonTakeoff');
-		let buttonGarage = this.add.image(config.width / 2, config.height / 3 + 100, 'buttonGarage');
-		let buttonSettings = this.add.image(config.width / 2, config.height / 3 + 200, 'buttonSettings');
-		let buttonFEARS = this.add.image(config.width / 2, config.height / 3 + 300, 'buttonFEARS');
+		let buttonTakeoff = this.add.image(config.width / 2, config.height / 4, 'buttonTakeoff');
+		let buttonGarage = this.add.image(config.width / 2, config.height / 4 + 100, 'buttonGarage');
+		let buttonSettings = this.add.image(config.width / 2, config.height / 4 + 200, 'buttonSettings');
+		let buttonFEARS = this.add.image(config.width / 2, config.height / 4 + 300, 'buttonFEARS');
 		
 		let buttonsMainMenu = [buttonTakeoff,buttonGarage,buttonSettings,buttonFEARS];
 		buttonsMainMenu.forEach(button => button.setInteractive());
