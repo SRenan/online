98af4edd8a29af5662b8f35ed95020596da15b17
dubiousdata <99302753+dubiousdata@users.noreply.github.com>
2024-05-29 14:17:05 -0400

    Update Scene2_mainMenu.js

--- TurquiePeutOnCompter/Scene2_mainMenu.js
+++ TurquiePeutOnCompter/Scene2_mainMenu.js
@@ -8,7 +8,7 @@ class Scene2 extends Phaser.Scene{
 		//Current plane or something.
 		const currentAirplane = localStorage.getItem('currentAirplane') || 'airplane.png';
 
-		let hangarPreview = this.add.image(config.width / 2, config.height / 3 - 200, currentAirplane);
+		let hangarPreview = this.add.image(config.width / 2, config.height / 3 + 400, currentAirplane);
 
 		
 		// Add a button
