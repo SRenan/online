213714d3d0c469e79325ec5a13692c6a55a7ede7
dubiousdata <99302753+dubiousdata@users.noreply.github.com>
2024-05-29 15:37:15 -0400

    Update Scene4_hangar.js

--- TurquiePeutOnCompter/Scene4_hangar.js
+++ TurquiePeutOnCompter/Scene4_hangar.js
@@ -16,7 +16,7 @@ class Scene4 extends Phaser.Scene{
 		this.anims.create({
 			key: 'smoke_effect',
 			frames: this.anims.generateFrameNumbers('smoke_sheet', {start: 0, end: 6}),
-			frameRate: 20,
+			frameRate: 5,
 			repeat: 0
 		});
 		
